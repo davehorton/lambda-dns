d646f1033a641120fe28e573006d887f85172b07
Dave Horton <daveh@beachdognet.com>
2016-12-03 12:09:07 -0500

    fix timing

--- DnsQuery.js
+++ DnsQuery.js
@@ -1,14 +1,12 @@
 'use strict' ;
 var dns = require('dns') ;
 exports.handler = function( event, context, callback ) {
-  console.log('hostname %s: ', event.hostname) ;
-  console.log('type: %s: ', event.type) ;
+  console.log('dns check for %s:%s', event.hostname, event.type) ;
 
   var t = process.hrtime() ;
   dns.resolve( event.hostname, event.type, function(err, addresses) {
-
     t = process.hrtime(t) ;
-    var ms = (t[0] * 1000 + t[1] / 1000000).toFixed(3) ;
+    var ms = ((t[0] * 1000) + (t[1] / 1000000)).toFixed(2);
     if( err ) { 
       console.log('error with dns check %s:%s - %s', event.hostname, event.type, JSON.stringify(err)) ;
       return callback( null, {
@@ -18,6 +16,7 @@ exports.handler = function( event, context, callback ) {
       }); 
     }
 
+    console.log('dns %s:%s returned in %d ms, result: ', event.hostname, event.type, ms, JSON.stringify(addresses));
     return callback(null, {
       success: true,
       duration: ms,
