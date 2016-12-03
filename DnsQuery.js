d926024abb9fa98cd8ee6472e1c7e370cc8977db
Dave Horton <daveh@beachdognet.com>
2016-12-03 11:38:22 -0500

    remove package.json as its not needed

--- DnsQuery.js
+++ DnsQuery.js
@@ -4,14 +4,27 @@ exports.handler = function( event, context, callback ) {
   console.log('hostname %s: ', event.hostname) ;
   console.log('type: %s: ', event.type) ;
 
+  var t = process.hrtime() ;
   dns.resolve( event.hostname, event.type, function(err, addresses) {
 
+    t = process.hrtime(t) ;
+    var ms = (t[0] * 1000 + t[1] / 1000000).toFixed(3) ;
     if( err ) { 
       console.log('error with dns check %s:%s - %s', event.hostname, event.type, JSON.stringify(err)) ;
-      return callback( err ); 
+      return callback( null, {
+        success: false,
+        error: err,
+        duration: ms
+      }); 
     }
 
-    return callback(null, addresses); 
+    return callback(null, {
+      success: true,
+      duration: ms,
+      details: {
+        addresses: addresses
+      }
+    }); 
 
   }) ;
 } ;
